952939b4df696dd2e20f3132994d0b21d4b14f6e
petarpetreski24 <petarpetreski24@gmail.com>
2025-02-01 22:23:03 +0100

    Added functions and changes in the ui for the user applications

--- src/main/java/mk/ukim/finki/mendo/service/ApplicationService.java
+++ src/main/java/mk/ukim/finki/mendo/service/ApplicationService.java
@@ -7,7 +7,8 @@ import mk.ukim.finki.mendo.model.enums.Grade;
 import java.util.List;
 
 public interface ApplicationService {
-    Application registerForCompetition(Long cycleId, Grade grade, Long schoolId);
+    Application registerForCompetitionCycle(Long cycleId, Grade grade, Long schoolId);
+    Application registerForCompetition(Long competitionId, Grade grade, Long schoolId);
     public boolean isUserAlreadyRegisteredOnCycle(Long cycleId);
     List<Application> getAllApplicationsBySchoolId(Long schoolId);
     public List<Application> getAllApplicationsForTeacherSchoolsAndNotConfirmed(List<School> teacherSchools);
