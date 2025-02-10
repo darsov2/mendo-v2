b5b6638d69be3b3ebc725585826fa40516c0525d
petarpetreski24 <petarpetreski24@gmail.com>
2025-02-10 19:41:37 +0100

    minor fixes

--- src/main/java/mk/ukim/finki/mendo/service/impl/CompetitionServiceImpl.java
+++ src/main/java/mk/ukim/finki/mendo/service/impl/CompetitionServiceImpl.java
@@ -3,6 +3,7 @@ package mk.ukim.finki.mendo.service.impl;
 import mk.ukim.finki.mendo.model.*;
 import mk.ukim.finki.mendo.model.enums.CompetitionTypes;
 import mk.ukim.finki.mendo.repository.CompetitionRepository;
+import mk.ukim.finki.mendo.repository.CompetitionTaskRepository;
 import mk.ukim.finki.mendo.repository.ParticipationRepository;
 import mk.ukim.finki.mendo.repository.RoomsRepository;
 import mk.ukim.finki.mendo.service.CompetitionCycleService;
@@ -13,10 +14,7 @@ import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @Service
@@ -27,15 +25,17 @@ public class CompetitionServiceImpl implements CompetitionService {
     private final RoomsRepository roomsRepository;
     private final TaskService taskService;
     private final CompetitionTaskService competitionTaskService;
+    private final CompetitionTaskRepository competitionTaskRepository;
 
 
-    public CompetitionServiceImpl(CompetitionCycleService service, CompetitionRepository competitionRepository, ParticipationRepository participationRepository, RoomsRepository roomsRepository, TaskService taskService, CompetitionTaskService competitionTaskService) {
+    public CompetitionServiceImpl(CompetitionCycleService service, CompetitionRepository competitionRepository, ParticipationRepository participationRepository, RoomsRepository roomsRepository, TaskService taskService, CompetitionTaskService competitionTaskService, CompetitionTaskRepository competitionTaskRepository) {
         this.competitionCycleService = service;
         this.competitionRepository = competitionRepository;
         this.participationRepository = participationRepository;
         this.roomsRepository = roomsRepository;
         this.taskService = taskService;
         this.competitionTaskService = competitionTaskService;
+        this.competitionTaskRepository = competitionTaskRepository;
     }
 
     @Override
@@ -162,20 +162,47 @@ public class CompetitionServiceImpl implements CompetitionService {
                     competition.setDeadline(deadline);
                     competition.setRooms(roomsList);
 
-                    List<CompetitionTask> competitionTasks = new ArrayList<>();
-                    for (int i = 0; i < taskIds.size(); i++) {
-                        Task task = taskService.getTaskById(taskIds.get(i));
-                        CompetitionTask competitionTask = new CompetitionTask(
-                                taskPoints.get(i).intValue(),
-                                type,
-                                task.getText(),
-                                competition,
-                                task
-                        );
-                        competitionTasks.add(competitionTask);
-                    }
-                    competition.setTasks(competitionTasks);
+                    Set<Long> existingTaskIds = competition.getTasks().stream()
+                            .map(task -> task.getTask().getId())
+                            .collect(Collectors.toSet());
 
+                    List<CompetitionTask> tasksToRemove = competition.getTasks().stream()
+                            .filter(task -> !taskIds.contains(task.getTask().getId()))
+                            .collect(Collectors.toList());
+
+                    competition.getTasks().removeAll(tasksToRemove);
+                    competitionTaskRepository.deleteAll(tasksToRemove);
+
+                    Map<Long, CompetitionTask> existingTasks = competition.getTasks().stream()
+                            .collect(Collectors.toMap(
+                                    task -> task.getTask().getId(),
+                                    task -> task
+                            ));
+
+                    List<CompetitionTask> updatedTasks = new ArrayList<>();
+
+                    for (int i = 0; i < taskIds.size(); i++) {
+                        Long taskId = taskIds.get(i);
+
+                        if (existingTasks.containsKey(taskId)) {
+                            CompetitionTask existingTask = existingTasks.get(taskId);
+                            existingTask.setPoints(taskPoints.get(i).intValue());
+                            updatedTasks.add(existingTask);
+                        } else {
+
+                            Task task = taskService.getTaskById(taskId);
+                            CompetitionTask competitionTask = new CompetitionTask(
+                                    taskPoints.get(i).intValue(),
+                                    type,
+                                    task.getText(),
+                                    competition,
+                                    task
+                            );
+                            updatedTasks.add(competitionTask);
+                        }
+                    }
+
+                    competition.setTasks(updatedTasks);
                     return competitionRepository.save(competition);
                 });
     }
