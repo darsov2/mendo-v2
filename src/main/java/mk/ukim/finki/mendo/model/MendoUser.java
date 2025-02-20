90e58a46e13929c7f16e258de5ebe609a8652312
David <darsov2@gmail.com>
2025-02-20 16:55:27 +0100

    eager roles

--- src/main/java/mk/ukim/finki/mendo/model/MendoUser.java
+++ src/main/java/mk/ukim/finki/mendo/model/MendoUser.java
@@ -1,6 +1,7 @@
 package mk.ukim.finki.mendo.model;
 
 import jakarta.persistence.*;
+import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,15 +10,13 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 @Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Transactional
 public class MendoUser extends BaseAuditedEntity<Long> implements UserDetails {
     Boolean isTeacher;
     String username;
@@ -30,16 +29,16 @@ public class MendoUser extends BaseAuditedEntity<Long> implements UserDetails {
     String email;
     @ManyToOne
     School studiesSchool;
-    @ManyToMany
+    @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(name = "schools_teaches",
     joinColumns = @JoinColumn(name = "user_id"),
     inverseJoinColumns = @JoinColumn(name = "school_id"))
-    List<School> teachesSchools;
+    List<School> teachesSchools = new ArrayList<>();
     Boolean isAccountNonExpired = true;
     Boolean isAccountNonLocked = true;
     Boolean isCredentialsNonExpired = true;
     Boolean isEnabled = true;
-    @ManyToMany(fetch = FetchType.LAZY)
+    @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(
             name = "user_roles",
             joinColumns = @JoinColumn(name = "user_id"),
